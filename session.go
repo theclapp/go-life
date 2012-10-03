f0427c66cc0342b65be4537c9f0fb98a0f6eeafa
Larry Clapp <larry@theclapp.org>
2012-10-02 22:07:33 -0400

    Split web.go into web.go and session.go.
    All session locking is now in session.go.

---
+++ session.go
@@ -0,0 +1,137 @@
+package main
+
+// vim:sw=3:ts=3:fdm=indent
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+type sessionId string
+
+type session struct {
+	sid        sessionId
+	nextPageId int
+	// map pageIds to listener channels
+	listeners map[string]chan string
+	delay     float32
+	stop      int
+	uCh       chan *universe
+	u         *universe
+	numU      int
+	gen       int
+	sync.Mutex
+}
+
+var sessions = make(map[sessionId]*session)
+var sessionsLock sync.Mutex
+var nextSessionNum = 0
+
+// FIXME Locks sessions[]: add a new session.
+func getSession(w http.ResponseWriter, req *http.Request) *session {
+	var sid sessionId
+	sessionsLock.Lock()
+	defer sessionsLock.Unlock()
+	sessionCookie, err := req.Cookie("session")
+	if err == nil {
+		sid = sessionId(sessionCookie.Value)
+		if sessions[sid] == nil {
+			fmt.Printf("%s: Invalid session cookie: %s\n", curFuncName(1), sid)
+		} else {
+			return sessions[sid]
+		}
+	}
+
+	fmt.Printf("%s: No session cookie; setting %d\n", curFuncName(1), nextSessionNum)
+	sid = sessionId(fmt.Sprintf("%d", nextSessionNum))
+	sessionCookie = &http.Cookie{
+		Name:   "session",
+		Value:  string(sid),
+		MaxAge: 3600,
+	}
+	http.SetCookie(w, sessionCookie)
+	sessions[sid] = &session{
+		sid:       sid,
+		listeners: make(map[string]chan string),
+		delay:     800.00,
+		uCh:       make(chan *universe),
+	}
+	nextSessionNum++
+	go func(s *session) {
+		u := makeUniverse()
+		for {
+			s.uCh <- u
+			u = nextGen(u)
+		}
+	}(sessions[sid])
+	return sessions[sid]
+}
+
+// FIXME Locks the session: get the next PageId
+func (s *session) getNextPageId() (result string) {
+	s.Lock()
+	defer s.Unlock()
+	result = fmt.Sprintf("%d", s.nextPageId)
+	s.nextPageId++
+	return
+}
+
+// FIXME Locks the session: Get the next Universe.
+func (s *session) nextU(pageId string) *universe {
+	s.Lock()
+	defer s.Unlock()
+	if s.numU < len(s.listeners) {
+		s.numU++
+	} else {
+		s.u = <-s.uCh
+		s.gen++
+		s.numU = 1
+	}
+	fmt.Printf("session: %s, pageId: %s, gen: %d, numU: %d, #listeners: %d\n",
+		s.sid, pageId, s.gen, s.numU, len(s.listeners))
+	return s.u
+}
+
+// FIXME Locks the session, and sometimes sessions[]: change delay and/or
+// stop, or delete the session entirely.
+func (s *session) pressButton(w http.ResponseWriter, whichButton string) {
+	s.Lock()
+	defer s.Unlock()
+	fmt.Printf("title is %s\n", whichButton)
+	switch whichButton {
+	case "delayMore":
+		s.delay *= 2
+	case "delayLess":
+		s.delay /= 2
+	case "stopLife":
+		s.stop = 1
+		s.delay++
+	case "startLife":
+		s.stop = 0
+		s.delay--
+	case "clearSession":
+		sessionsLock.Lock()
+		delete(sessions, s.sid)
+		sessionsLock.Unlock()
+		http.SetCookie(w, &http.Cookie{
+			Name:   "session",
+			MaxAge: -1,
+		})
+	}
+}
+
+// FIXME Locks the session: add a new listener
+func (s *session) addListener(pageId string) chan string {
+	s.Lock()
+	defer s.Unlock()
+	s.listeners[pageId] = make(chan string)
+	return s.listeners[pageId]
+}
+
+// FIXME Locks the session: delete the listener when the event's sent.
+func (s *session) removeListener(pageId string) {
+	s.Lock()
+	defer s.Unlock()
+	delete(s.listeners, pageId)
+}
--- web.go
+++ web.go
@@ -8,30 +8,9 @@ import (
 	"image/png"
 	"net/http"
 	"runtime"
-	"sync"
 )
 
-type sessionId string
-
-type session struct {
-	sid        sessionId
-	nextPageId int
-	// map pageIds to listener channels
-	listeners map[string]chan string
-	delay     float32
-	stop      int
-	uCh       chan *universe
-	u         *universe
-	numU      int
-	gen       int
-	sync.Mutex
-}
-
-var sessions = make(map[sessionId]*session)
-var sessionsLock sync.Mutex
 var templates = make(map[string]*template.Template)
-
-var nextSessionNum = 0
 
 var numCPU = runtime.NumCPU()
 
@@ -68,49 +47,22 @@ func LifeServer(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-// FIXME Locks the session
-func (s *session) getNextPageId() (result string) {
-	s.Lock()
-	defer s.Unlock()
-	result = fmt.Sprintf("%d", s.nextPageId)
-	s.nextPageId++
-	return
-}
-
 func LifeJS(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("Serving LifeJS")
 	http.ServeFile(w, req, "life.js")
 }
 
-// FIXME Locks the session.
 func LifeImage(w http.ResponseWriter, req *http.Request) {
-	pageId := req.FormValue("pageId")
-	s := getSession(w, req)
-	s.Lock()
-	if s.numU < len(s.listeners) {
-		s.numU++
-	} else {
-		s.u = <-s.uCh
-		s.gen++
-		s.numU = 1
-	}
-	curU := s.u
-	fmt.Printf("session: %s, pageId: %s, gen: %d, numU: %d, #listeners: %d\n",
-		s.sid, pageId, s.gen, s.numU, len(s.listeners))
-	s.Unlock()
 	// FIXME this renders the image repeatedly and is quite inefficient
-	png.Encode(w, display(curU))
+	png.Encode(w, display(getSession(w, req).nextU(req.FormValue("pageId"))))
 }
 
-// FIXME Locks the session, and sometimes sessions[]
 func Button(w http.ResponseWriter, req *http.Request) {
 	err := req.ParseForm()
 	if err != nil {
 		fmt.Println("Could not parse form in Button()")
 		return
 	}
-	whichButton := req.FormValue("title")
-	fmt.Printf("title is %s\n", whichButton)
 
 	s := getSession(w, req)
 
@@ -119,28 +71,7 @@ func Button(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	s.Lock()
-	switch whichButton {
-	case "delayMore":
-		s.delay *= 2
-	case "delayLess":
-		s.delay /= 2
-	case "stopLife":
-		s.stop = 1
-		s.delay++
-	case "startLife":
-		s.stop = 0
-		s.delay--
-	case "clearSession":
-		sessionsLock.Lock()
-		delete(sessions, s.sid)
-		sessionsLock.Unlock()
-		http.SetCookie(w, &http.Cookie{
-			Name:   "session",
-			MaxAge: -1,
-		})
-	}
-	s.Unlock()
+	s.pressButton(w, req.FormValue("title"))
 
 	event := fmt.Sprintf(`refresh({"delay":%f,"stop":%d})`, s.delay, s.stop)
 	fmt.Println("event is " + event)
@@ -152,17 +83,12 @@ func Button(w http.ResponseWriter, req *http.Request) {
 // Long-polled URL.  What happens if the connection times out, or is closed?
 // Indeed, that's exactly what happens (the socket is closed) if you press
 // reload on the browser.
-// FIXME Locks the session.
 func Updates(w http.ResponseWriter, req *http.Request) {
 	s := getSession(w, req)
 	pageId := req.FormValue("pageId")
 	fmt.Printf("starting Updates, pageId is %s, req is %p\n", pageId, req)
-	listener := make(chan string)
-	s.Lock()
-	s.listeners[pageId] = listener
-	s.Unlock()
 
-	event := <-listener
+	event := <-s.addListener(pageId)
 	fmt.Printf("event recieved for req %p, pageId %s; event is %s\n", req, pageId, event)
 	_, err := w.Write([]byte(event))
 	if err != nil {
@@ -170,47 +96,5 @@ func Updates(w http.ResponseWriter, req *http.Request) {
 	}
 	fmt.Printf("Updates finished for req %p, pageId %s\n", req, pageId)
 
-	s.Lock()
-	delete(s.listeners, pageId)
-	s.Unlock()
-}
-
-// FIXME locks sessions[]
-func getSession(w http.ResponseWriter, req *http.Request) *session {
-	var sid sessionId
-	sessionsLock.Lock()
-	defer sessionsLock.Unlock()
-	sessionCookie, err := req.Cookie("session")
-	if err == nil {
-		sid = sessionId(sessionCookie.Value)
-		if sessions[sid] == nil {
-			fmt.Printf("%s: Invalid session cookie: %s\n", curFuncName(1), sid)
-		} else {
-			return sessions[sid]
-		}
-	}
-
-	fmt.Printf("%s: No session cookie; setting %d\n", curFuncName(1), nextSessionNum)
-	sid = sessionId(fmt.Sprintf("%d", nextSessionNum))
-	sessionCookie = &http.Cookie{
-		Name:   "session",
-		Value:  string(sid),
-		MaxAge: 3600,
-	}
-	http.SetCookie(w, sessionCookie)
-	sessions[sid] = &session{
-		sid:       sid,
-		listeners: make(map[string]chan string),
-		delay:     800.00,
-		uCh:       make(chan *universe),
-	}
-	nextSessionNum++
-	go func(s *session) {
-		u := makeUniverse()
-		for {
-			s.uCh <- u
-			u = nextGen(u)
-		}
-	}(sessions[sid])
-	return sessions[sid]
+	s.removeListener(pageId)
 }
