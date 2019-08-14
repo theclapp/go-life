9a6e1de3ce11e8e7906675531f85e7df43015fee
Larry Clapp <larry@theclapp.org>
2019-08-14 10:24:45 -0400

    Parallelize NextGen; FPS counter; single-step; etc
    - More or less for kicks, parallelize NextGen().  Still suboptimal,
      though.  Left old code in because I think it's easier to understand,
      and might want to easily go back to it, or do a/b comparisons.
    - Add an FPS counter
    - Added "s" for single-stepping (mostly useful while paused, but can
      also short-circuit the timer).
    - Change default scale from 10 to 3.  Change initial scroll to show some
      borders around the initial universe.
    - Account for DestroyEvent

--- life.go
+++ life.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"log"
 	"math/rand"
 	"os"
+	"sync"
 	"time"
 
 	"gioui.org/ui"
@@ -24,7 +26,9 @@ import (
 )
 
 const (
-	fontHeight = 14
+	fontHeight  = 14
+	univSq      = 200
+	randDensity = 333
 )
 
 type (
@@ -52,18 +56,23 @@ type (
 		genTimer *time.Ticker
 
 		buttons map[string]*ggesture.Click
+
+		fps, numFrames int
+		fpsTimer       *time.Ticker
 	}
 )
 
 func main() {
 	go func() {
 		w := &Window{
-			scale:   10,
+			scale:   3,
 			buttons: map[string]*ggesture.Click{},
+			scrollX: univSq,
+			scrollY: univSq,
 		}
 		w.u = NewUniverse()
 		// w.u.RPentomino()
-		w.u.Random(100, 100, 333)
+		w.u.Random(univSq, univSq, randDensity)
 
 		w.w = app.NewWindow(&app.WindowOptions{
 			Width:  ui.Dp(800),
@@ -77,13 +86,61 @@ func main() {
 
 		w.interval = 100 * time.Millisecond
 		w.genTimer = time.NewTicker(w.interval)
+		w.fpsTimer = time.NewTicker(time.Second)
+
+		var u *Universe
+		doRand := make(chan bool)
+		// Queue up generations as fast as we can
+		uCh := make(chan *Universe, 10)
+		go func() {
+			for {
+				select {
+				case <-doRand:
+				DRAIN:
+					for {
+						select {
+						case <-uCh:
+						default:
+							break DRAIN
+						}
+					}
+					newU := NewUniverse()
+					newU.Random(univSq, univSq, randDensity)
+					u = newU
+					w.u = newU.NextGen()
+					w.w.Invalidate()
+				case uCh <- w.u:
+					w.u = w.u.NextGen()
+				}
+			}
+		}()
+
 		for {
 			select {
-			case <-w.genTimer.C:
-				w.u = w.u.NextGen()
+			case <-w.fpsTimer.C:
+				w.fps = w.numFrames
+				w.numFrames = 0
 				w.w.Invalidate()
-			case e := <-w.w.Events():
+			// Would like some way to ignore this if uCh is empty; otherwise we
+			// just spin in this loop wasting CPU.  (This can happen if the
+			// genTimer is going a lot faster than we're producing generations.)
+			case <-w.genTimer.C:
+				select {
+				case u = <-uCh:
+					w.numFrames++
+					w.w.Invalidate()
+				default:
+				}
+			case e, ok := <-w.w.Events():
+				if !ok {
+					return
+				}
 				switch e := e.(type) {
+				case app.DestroyEvent:
+					if err := e.Err; err != nil {
+						log.Fatal(err)
+					}
+					return
 				case key.EditEvent:
 					switch e.Text {
 					case "q":
@@ -94,8 +151,7 @@ func main() {
 						w.cont()
 					// Restart with another random universe
 					case "r":
-						w.u.Random(100, 100, 333)
-						w.w.Invalidate()
+						doRand <- true
 					case "-":
 						w.zoomOut()
 					case "+", "=":
@@ -104,12 +160,19 @@ func main() {
 						w.goFaster()
 					case "<", ",":
 						w.goSlower()
+					case "s": // single-step
+						select {
+						case u = <-uCh:
+							w.numFrames++
+							w.w.Invalidate()
+						default:
+						}
 					default:
 						// fmt.Printf("key name: %s\n", e.Text)
 					}
 				case app.UpdateEvent:
 					ops.Reset()
-					w.Layout(e, ops)
+					w.Layout(ops, e, u)
 					w.w.Update(ops)
 				default:
 					// fmt.Printf("event %T %+v\n", e, e)
@@ -150,11 +213,108 @@ func (u *Universe) IsSet(p Pos) int {
 	return 0
 }
 
+const chanLen = 1000
+
 func (u *Universe) NextGen() *Universe {
+	next := *u
+	next.cells = make(map[Pos]bool, len(u.cells))
+	next.gen = u.gen + 1
+
+	liveCh := make(chan Pos, chanLen)
+	skipCheckedCh := make(chan Pos, chanLen)
+	neighborCh := make(chan Pos, chanLen)
+	newCh := make(chan Pos, chanLen)
+
+	newChWg := sync.WaitGroup{}
+	wg := sync.WaitGroup{}
+
+	for n := 0; n < 2; n++ {
+		newChWg.Add(1)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			defer newChWg.Done()
+			for pos := range liveCh {
+				// Will this cell be alive in the next generation?
+				if n := u.NumNeighbors(pos); n == 2 || n == 3 {
+					newCh <- pos
+				}
+			}
+		}()
+	}
+
+	// Can only be one checker goroutine
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		checked := make(map[Pos]bool, len(u.cells))
+		for pos := range skipCheckedCh {
+			if checked[pos] {
+				continue
+			}
+			neighborCh <- pos
+			checked[pos] = true
+		}
+		close(neighborCh)
+	}()
+
+	for n := 0; n < 2; n++ {
+		newChWg.Add(1)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			defer newChWg.Done()
+			// Should this cell be alive in the next generation?
+			for pos := range neighborCh {
+				if u.NumNeighbors(pos) == 3 {
+					newCh <- pos
+				}
+			}
+		}()
+	}
+	go func() {
+		newChWg.Wait()
+		close(newCh)
+	}()
+
+	// Only one of these, too
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for pos := range newCh {
+			next.cells[pos] = true
+		}
+	}()
+
+	for pos := range u.cells {
+		// Check for staying alive
+		liveCh <- pos
+
+		// Check for births
+		xm1, xp1 := pos.x-1, pos.x+1
+		ym1, yp1 := pos.y-1, pos.y+1
+		for _, neighbor := range []Pos{
+			{xm1, ym1}, {pos.x, ym1}, {xp1, ym1},
+			{xm1, pos.y}, {xp1, pos.y},
+			{xm1, yp1}, {pos.x, yp1}, {xp1, yp1}} {
+			if !u.cells[neighbor] {
+				skipCheckedCh <- neighbor
+			}
+		}
+	}
+	close(liveCh)
+	close(skipCheckedCh)
+
+	wg.Wait()
+
+	return &next
+}
+
+func (u *Universe) NextGenOld() *Universe {
 	next := *u
 	next.cells = make(map[Pos]bool)
 	next.gen = u.gen + 1
-	checked := make(map[Pos]bool)
+	checked := make(map[Pos]bool, len(u.cells))
 	for pos := range u.cells {
 		// Will this cell be alive in the next generation?
 		if n := u.NumNeighbors(pos); n == 2 || n == 3 {
@@ -171,10 +331,10 @@ func (u *Universe) NextGen() *Universe {
 			if next.cells[neighbor] || checked[neighbor] {
 				continue
 			}
+			checked[neighbor] = true
 			if u.NumNeighbors(neighbor) == 3 {
 				next.Set(neighbor)
 			}
-			checked[neighbor] = true
 		}
 	}
 	return &next
@@ -216,7 +376,11 @@ func max(n1, n2 int) int {
 	return n2
 }
 
-func (w *Window) Layout(e app.UpdateEvent, ops *ui.Ops) {
+func (w *Window) Layout(ops *ui.Ops, e app.UpdateEvent, u *Universe) {
+	if u == nil {
+		return
+	}
+
 	cfg := &e.Config
 	w.faces.Reset(cfg)
 	cs := layout.Constraints{
@@ -259,7 +423,8 @@ func (w *Window) Layout(e app.UpdateEvent, ops *ui.Ops) {
 
 	lbl := text.Label{
 		Face: w.faces.For(w.regular, ui.Sp(fontHeight)),
-		Text: fmt.Sprintf(" Gen: %d | Scale: %d | GenTime: %v", w.u.gen, w.scale, w.interval),
+		Text: fmt.Sprintf(" Gen: %d | Scale: %d | GenTime: %v | FPS: %d (%0.2f)",
+			u.gen, w.scale, w.interval, w.fps, float32(time.Second)/float32(w.interval)),
 	}
 	if w.paused {
 		lbl.Text += " | PAUSED"
@@ -279,7 +444,7 @@ func (w *Window) Layout(e app.UpdateEvent, ops *ui.Ops) {
 		Y: float32(w.scrollY),
 	}).Add(ops)
 	paint.ColorOp{Color: color.RGBA{A: 0xff, G: 0xff}}.Add(ops)
-	for pos := range w.u.cells {
+	for pos := range u.cells {
 		paint.PaintOp{
 			Rect: f32.Rectangle{
 				Min: f32.Point{float32(w.scale * pos.x), float32(w.scale * pos.y)},
@@ -291,7 +456,6 @@ func (w *Window) Layout(e app.UpdateEvent, ops *ui.Ops) {
 }
 
 func (u *Universe) Random(x, y, density int) {
-	*u = *NewUniverse()
 	for px := 0; px < x; px++ {
 		for py := 0; py < y; py++ {
 			if rand.Intn(1000) < density {
